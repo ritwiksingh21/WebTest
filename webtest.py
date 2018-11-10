1c7028989f6d5a5d762f39a879cf495a291c86cb
unknown <allegracopland@brandeis.edu>
2018-11-10 13:46:45 -0500

    yes

--- webtest.py
+++ webtest.py
@@ -1,3 +1,21 @@
 import subprocess
+from bs4 import BeautifulSoup
 
 subprocess.call(["pandoc", "-o", "output.html", "test.docx"], shell=True) #convert text to markup in output.html
+
+with open("output.html") as fp:
+    soup = BeautifulSoup(fp)
+
+def convertToForm(soup):
+    questions_blocks = soup.find_all("ol", type=1) #find all ols with numbers
+    for block in questions_blocks:
+        questions = block.find_all("li")
+        for question in questions:
+            new_field = soup.new_tag("textarea")
+            question.p.append(new_field)
+    print(questions)
+    f = open("output.html", "w+")
+    f.write('')
+    f.write(block.prettify())
+
+convertToForm(soup)
