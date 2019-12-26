e636beb87fb985b150a3ad9f2169df59bd6fa18c
snickercop <allegracopland@brandeis.edu>
2019-12-25 22:31:23 -0500

    cleanup + prototype Question model

--- wtdjango/questions/models.py
+++ wtdjango/questions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class MC_Answer(models.Model):
+    text = models.CharField(max_length=255)
+    letter = models.CharField(max_length=1)
+
+class Question(models.Model):
+    text = models.CharField(max_length=511)
+    multiple_choice = models.BooleanField()
+    lettered_parts = models.BooleanField()
+    mc_answers = models.ManyToManyField(MC_Answer)
+    mc_correct = models.ForeignKey(MC_Answer, models.SET_NULL, blank=True, null=True, related_name="mc_answers")
